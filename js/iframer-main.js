9c78dd5fe750b5b368653f53b02e10da47a8b024
Nathan Lawrence <lawrencen@missouri.edu>
2015-12-16 11:49:07 -0600

    Added Tabletop, fixed basic links, added test code for tabletop.

---
+++ js/iframer-main.js
@@ -0,0 +1,14 @@
+window.onload = function() { init() };
+
+  var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE&output=html';
+
+  function init() {
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    alert("Successfully processed!")
+    console.log(data);
+  }
