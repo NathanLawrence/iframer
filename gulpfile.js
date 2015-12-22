7e9a66de180947af0ac5bc88be55d370360a015f
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2015-12-21 23:42:30 -0600

    Gulping a little more down
    Added more Travis, and gulp has HTML and semicolons!

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	concat = require('gulp-concat');
 
-var jsSources = ['components/js/iframer-vars.js', 'components/js/tabletop.js', 'components/js/iframer-main.js'];
+var jsSources = ['components/js/tabletop.js', 'components/js/iframer-vars.js', 'components/js/iframer-main.js'];
+var htmlSources = ['components/index.html'];
 
 gulp.task('compile-dev', function(){
 	gulp.src(jsSources)
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe(gulp.dest('builds/development/js'));
+	gulp.src(htmlSources)
+		.pipe(gulp.dest('builds/development'));
 });
