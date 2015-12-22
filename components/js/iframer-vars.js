021197a9919b4d3a9d81427c65964dc5d6d9ac4c
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2015-12-22 09:35:40 -0600

    I forgot the semicolon.

--- components/js/iframer-vars.js
+++ components/js/iframer-vars.js
@@ -1,1 +1,1 @@
-var sheetURL = "https://docs.google.com/spreadsheets/d/183BqQXLnw3ZtqGkB-d-bnFtk5PoPdx7gjmJocCzUeao/pubhtml"
+var sheetURL = "https://docs.google.com/spreadsheets/d/183BqQXLnw3ZtqGkB-d-bnFtk5PoPdx7gjmJocCzUeao/pubhtml";
